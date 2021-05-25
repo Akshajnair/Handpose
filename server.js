9aef68ccab8bbb58e2abdb9b622245e981dc54b4
Akshaj Nair <34983749+Akshajnair@users.noreply.github.com>
2021-05-25 21:53:04 +0530

    initial commit

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
